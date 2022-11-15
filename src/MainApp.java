5f87e6e1c03571ee35587b98dd0bfb76379c7808
Valeriya <vi.barreau2011@gmail.com>
2022-11-15 12:06:33 +0100

    testes sout

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,5 +2,8 @@ public class  MainApp {
     public static void main(String[] args) {
         //initialiser les cartes avec leur id et matrice
         //lancer la console
+
+        System.out.println("hello");
+        System.out.println();
     }
 }
