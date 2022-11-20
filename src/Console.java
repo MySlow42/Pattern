9f4fc0a8df3a1bb7f71387af17fa71bc94b04078
Vival29 <virginie.f.r.valentin@gmail.com>
2022-11-20 09:34:10 +0100

    Started Console features

--- src/Console.java
+++ src/Console.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class Console {
     final private String DEL = "del";
@@ -16,15 +16,20 @@ public class Console {
     //meneur de jeux entre q d ou c pour checker quine double quine carton, dans le cas carton ok reinitaliser le jeu
     // automatique ?
 
-    public void runJeux(){
+    public void runJeux() {
         //TODO Apparence de sout pour que le meneur de jeu entre les numéros + indication de lettre quand veut checker, + indication de lettre quand erreur a refaire?
         System.out.println("____________________________________________________________________");
         System.out.println("Aide de commande :");
-        System.out.println("Si vous vous êtes trompé, taper \"del\");
-                System.out.println("____________________________________________________________________");
-        System.out.print("Bonne partie de Lotto!!");
-        System.out.println("            ");
-        System.out.println("Vous jouez la partie n°: " + compteur);
+        System.out.println("Entrez les numéros tiré les uns après les autres, en validant avec la touche enter");
+        System.out.println("Si vous vous êtes trompé, taper \"del\", cela supprimera le dernier numero entré");
+        System.out.println("Losrque vous voulez contrôler une QUINE taper \"v1\"");
+        System.out.println("Losrque vous voulez contrôler une DOUBLE-QUINE taper \"v2\"");
+        System.out.println("Losrque vous voulez contrôler un CARTON taper \"v3\"");
+        System.out.println("____________________________________________________________________");
+        System.out.println("Bonne partie de Lotto!!                              Vous jouez la partie n°: " + compteur);
+        Scanner command = new Scanner(System.in);
         System.out.println("Entrer le numéro tiré, puis pressez enter");
 
+
+    }
 }
